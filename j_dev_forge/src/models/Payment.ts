8ea71dedf947e2ce01a3c67a319b1a29570a0b47
ewert <mewert@dal.ca>
2023-11-14 19:25:00 -0400

    Saved the file. Thanks Crystal lol.

---
+++ j_dev_forge/src/models/Payment.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+import validator from 'validator' //https://www.npmjs.com/package/validator
+
+function validateURL(value: string) {
+    return validator.isURL(value, { require_tld: false });
+}
+
+export interface Payment extends mongoose.Document {
+    transaction_id: string
+    project_id: mongoose.Types.ObjectId; //References Projects
+    user_id: mongoose.Types.ObjectId; //References Users
+    amount: number
+    time_stamp: Date
+}
+
+const PaymentSchema = new mongoose.Schema<Payment>({
+    transaction_id:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    project_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Projects', //Reference to the Projects model
+        required: true,
+    },
+    user_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users', //Reference to the Users model
+        required: true,
+    },
+    amount:{
+        type: Number,
+        required: true,
+    },
+    time_stamp:{
+        type: Date,
+        required: true,
+    },
+})
+
+export default mongoose.models.Payment || mongoose.model<Payment>('Payment', PaymentSchema)
